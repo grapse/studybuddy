e7ccb76e20fccd1e2ce67ec6ec40d9e9e9bda854
grape <leah.ren@hotmail.com>
2022-01-16 01:31:23 -0700

    fixed formatting, navbar

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,17 +1,57 @@
-import { useState } from "react";
-import { Navbar, Container, Nav} from 'react-bootstrap';
 
-function Navigation({imgdata}){
+import { Navbar, Container, Nav, Button} from 'react-bootstrap';
+import styled from 'styled-components';
+
+const NavItem = styled.div`
+    position:relative;
+    &:after{
+      display: block;
+      content: '';
+      width: 0%;
+      height: 2px;
+      background: #db504a;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      transition:0.3s ease-in-out;
+    }
+    &:hover:after{
+
+      width: 100%;
+
+      transition:0.3s ease-in-out;
+    }
+    `
+const NavItemSelect = styled.div`
+    position:relative;
+    &:after{
+      display: block;
+      content: '';
+      width: 100%;
+      height: 2px;
+      background: #db504a;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      transition:0.3s ease-in-out;
+    }
+    `
+
+function Navigation({path}){
     return (     
     <> 
-        <Navbar bg="#fff" style={{height:"10vh",paddingLeft:"0px"}} expand="lg">
+        <Navbar bg="#fff" style={{height:"10vh",paddingLeft:"0px",boxShadow:"0px 0px 3px #888888"}} expand="lg">
           <Container>
-            <Navbar.Brand style={{color:"#db504a"}} href="/">StudyBuddy</Navbar.Brand>
+            <Navbar.Brand style={{color:"#db504a",fontWeight:"700"}} href="/">StudyBuddy</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
+                <Nav.Link href="/timer">{path === "/timer" ? <NavItemSelect>Timer</NavItemSelect> : <NavItem>Timer</NavItem>}</Nav.Link>
+                <Nav.Link href="/calendar">{path === "/calendar" ? <NavItemSelect>Calendar</NavItemSelect> : <NavItem>Calendar</NavItem>}</Nav.Link>
+                <Nav.Link href="/flashcards">{path === "/flashcards" ? <NavItemSelect>Flashcards</NavItemSelect> : <NavItem>Flashcards</NavItem>}</Nav.Link>
+              </Nav>
+              <Nav className="justify-content-end">
+                <Button style={{backgroundColor:"#db504a",borderColor:"#bb302a"}}>Log In</Button>
               </Nav>
             </Navbar.Collapse>
           </Container>
